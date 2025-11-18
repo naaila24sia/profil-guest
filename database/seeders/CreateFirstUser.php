5046ffcbd396f2bac0287230cf6c7e980b6c1efa
naaila24sia <naaila24si@mahasiswa.pcr.ac.id>
2025-11-18 17:55:24 +0700

    first user seeder

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,22 @@
+<?php
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name'     => 'Naaila',
+            'email'    => 'naaila24si@mahasiswa.pcr.ac.id',
+            'password' => Hash::make('Naaila24si'),
+        ]);
+
+    }
+}
